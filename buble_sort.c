61407fc58df1736e31f2e660634e587ae693fe4b
drspac@163.com <drspac@163.com>
2015-10-20 17:14:10 +0800

    some optimizing

--- buble_sort.c
+++ buble_sort.c
@@ -8,163 +8,124 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define NUMBER_SIZE 500000
+#define NUMBER_SIZE 100
 #define RANDOM_NUMBER_RANGE 1000
 
-int get_num(int sort[]);
-int buble_sort(int sort[]);
+int get_num(int sort[], int num);
 int generate_random_num(int num[], int num_size, int num_range);
 int generate_random_num_file(char *file_name, int num[], int number_size, int num_range);
-int get_random_num_from_file(char *file_name, int num_buf[], int num_counter);
+int get_random_num_from_file(char *file_name, int num_buf[], int num_counter, int max_num_length);
+int buble_sort(int sort[], int num_size);
 int merge_sort(int sort[], int start_index, int end_index);
-int merge(int sort[], int start_index, int mid_index, int end_index);
+static int merge(int sort[], int start_index, int mid_index, int end_index);
 
 int main(int argc, char **argv)
 {
 	int sort[NUMBER_SIZE] = {0};
-	int i = 0;
-	
+	memset(sort, '\0', sizeof(sort));
+
 	generate_random_num(sort, NUMBER_SIZE, RANDOM_NUMBER_RANGE);
-//	sleep(5);
 	generate_random_num_file("random_num.txt", sort, NUMBER_SIZE, RANDOM_NUMBER_RANGE);
-	printf("generate random num file done.\n sort: %s\n", sort);
-//	sleep(5);
+//	printf("generate random num file done.\n sort: %s\n", sort);
 
-//	memset(sort, '\0', sizeof(sort));
-//	get_random_num_from_file("random_num.txt", sort, NUMBER_SIZE);
-	printf("\nafter get sort:\n %s\n", sort);
-	for (i = 0; i < NUMBER_SIZE; i++) {
-		printf("%d\t", sort[i]);
-	}
-	printf("\n");
+	get_random_num_from_file("random_num.txt", sort, NUMBER_SIZE, 4);
 	
-	merge_sort(sort, 0, NUMBER_SIZE);
+	merge_sort(sort, 0, NUMBER_SIZE - 1);
 
+	int i = 0;
 	for (i = 0; i < NUMBER_SIZE; i++) {
 		printf("%d\t", sort[i]);
 	}
 	printf("\n");
 
-//	buble_sort(sort);
+//	buble_sort(sort_size);
 
 	return 0;
 }
 
-/*
- * get random number from file.
- * @num_buf: for random number store.
+/* get random number from file.
+ * @file_name:	 the name of file stored random number.
+ * @num_buf:	 for random number store.
  * @num_counter: get how many random number from file.
+ * @max_num_length: a number's max length.
  */
-int get_random_num_from_file(char *file_name, int num_buf[], int num_counter)
+int get_random_num_from_file(char *file_name, int num_buf[], int num_counter, int max_num_length)
 {
-	FILE *fd;
-	char store_buf[1024 * 4];
-	char *num_start = NULL;
-	char *num_end  = NULL;
-
-	int fd_low;
-	char number[10];
-	int num = 0;    /* as index: the x th number */
-	int number_length = 0; /* a number's length */
-	int number_get[1024];
-	
-	memset(number_get, '\0', sizeof(number_get));
-
-	while ((num_buf = malloc(1024 * 4)) == NULL) {
-		fputs("malloc fail while get random number from file.\n", stderr);
-		return 1;
-	}
-
-	memset(store_buf, '\0', 1024 * 4);
-
-	if ((fd = fopen(file_name, "r")) < 0) {
-		fputs("fopen error while get random num from file.\n", stderr);
-		return 2;
-	}
-
-	fgets(store_buf, sizeof(store_buf), fd);
-	printf("store buffer: %s\n", store_buf);
-
-	num_start = strstr((const char *)store_buf, "-");
-	num_end = strstr((const char *)num_start + 1, "-");
-	printf("in the beginning, num_start pointer: %s\nnum_end pointer: %s\n", num_start, num_end);
-
-	memset(store_buf, '\0', sizeof(store_buf));
-	if ((fd_low = open(file_name, O_RDWR)) < 0) {
+	int fd;
+	if ((fd = open(file_name, O_RDWR)) < 0) {
 		fputs("open fail.\n", stderr);
 		return 3;
 	}
-	number_length = 0;
-	while (read(fd_low, store_buf, 1) > 0) {
+
+	int number_length = 0; 	 	  /* a number's length */
+	char temp_buf[10];        	  /* for read() store */
+	char number[max_num_length + 1];                  /* for temp number store, store a number */
+	int number_get[num_counter + 1];      /* finally numbers get from file */
+	int num_index = 0; 		  	  /* as index: the x th number */
+	memset(temp_buf, '\0', 10);
+	memset(number, '\0', max_num_length);
+	memset(number_get, 0, sizeof(number_get));
+	while (read(fd, temp_buf, 1) > 0) {
 //		printf("char get: %s\n", store_buf);
-		if (store_buf[0] >= '0' && store_buf[0] <= '9') {
-			number[number_length] = store_buf[0];
-			number_length++;
-		} else if (store_buf[0] == '-') {
+		if (temp_buf[0] == '|' && number[0] != '\0') {
 			number[number_length] = '\0';
-			number_get[num] = atoi(number);
+			number_get[num_index] = atoi(number);
+			num_index++;
 //			printf("number get: %d\n", number_get[num]);
-			num++;
-			memset(number, '\0', sizeof(number));
+			memset(number, '\0', max_num_length);
 			number_length = 0;
+		} else if (temp_buf[0] >= '0' && temp_buf[0] <= '9') {
+			number[number_length] = temp_buf[0];
+			number_length++;
 		}
 	}
-	close(fd_low);
+	close(fd);
 
 	int i = 0;
-	for (i = 0; i < num; i++) {
+	printf("GET NUMBER FROM FILE:\n");
+	for (i = 0; i < num_index; i++) {
 		printf("%d\t", number_get[i]);
 	}
+	printf("\n-------------------------------\n");
 
-	for (i = 0; i < num; i++) {
-		num_buf[i] = number_get[i];
-		printf("num_buf[%d] = %d\n", i, num_buf[i]);
-	}
-//	memcpy(num_buf, number_get, sizeof(num_buf));	
-//	num_buf[0] = 22;
-	printf("num bf: %s\n", num_buf);
-
-/*	while ((fgets(store_buf, sizeof(store_buf), fd)) != NULL) {
-		memcpy(store_buf, num_start, num_end - num_start);
-		printf("store buffer: %s\n", store_buf);
-		num_start = strstr((const char *)store_buf, "-");
-		num_end = strstr((const char *)num_start + 1, "-");
-		printf("num_start pointer: %c\nnum_end pointer: %c\n", *num_start, *num_end);
-	}
-*/
-	
 	return 0;	
 }
  
-/*
- * generate random number and write into file.
- *  
+
+/* generate random number and write into file.
+ * @file_name:	file name
+ * @num: 	number array to store in file.
+ * @num_size:	array size.
+ * @num_range:	number range to max.
  */
 int generate_random_num_file(char *file_name, int num[], int num_size, int num_range)
 {
-	int i = 0;
-	int counter = 0;
 	char *store_buf = NULL;
 	FILE *fd = NULL;
 	
 
-	if ((store_buf = malloc(1024 * 100)) == NULL) {
+	/* num_size for '|' store and 1 for '\0' */
+	if ((store_buf = malloc(sizeof(int) * (num_size + 1) + 1)) == NULL) {
 		fputs("malloc fail.\n", stderr);
 		return 1;
 	}
+	memset(store_buf, '\0', sizeof(int) * (num_size + 1) + 1);
 	
 	if ((fd = fopen(file_name, "w+")) < 0) {
 		fputs("fopen error.\n", stderr);
+		return 2;
 	}
 
 	srandom(time(0));
-	for (i = 0; i < num_size; i++) {
-		num[i] = random() % num_range;
+	int iter = 0;
+	for (iter = 0; iter < num_size; iter++) {
+		num[iter] = random() % num_range;
 	}
 
 	printf("generated random num for file: \n");
-	for (i = 0; i < num_size; i++) {
-		printf("%d\t", num[i]);
+	int counter = 0;
+	for (iter = 0; iter < num_size; iter++) {
+		printf("%d\t", num[iter]);
 		counter++;
 		if (counter % 10 == 0) {
 			printf("\n");
@@ -172,71 +133,58 @@ int generate_random_num_file(char *file_name, int num[], int num_size, int num_r
 	}
 	printf("\n");
 	
-	sprintf(store_buf, "$");
-	for (i = 0; i < num_size; i++) {
-		sprintf(store_buf, "%s-%d", store_buf, num[i]);
+	for (iter = 0; iter < num_size; iter++) {
+		sprintf(store_buf, "%s|%d", store_buf, num[iter]);
 	}
-	printf("store buf = %s\n", store_buf);
+
+	printf("store buf =%s\n", store_buf);
 	fputs(store_buf, fd);
 	fclose(fd);
-	if (store_buf != NULL) {
-		free(store_buf);
-	}
+
+	free(store_buf);
 
 	return 0;
 }
 
 /* generate random number and stored in num[]
- * @num_size: how many random number you want.
- * @num_range: the max random number.
+ * @num_size: 	how many random number you want.
+ * @num_range: 	the max random number.
  */
 int generate_random_num(int num[], int num_size, int num_range)
 {
-	int i = 0;
-	int counter = 0;
-
+	int iter = 0;
 	srandom(time(0));
-	for (i = 0; i < num_size; i++) {
-		num[i] = random() % num_range;
+	for (iter = 0; iter < num_size; iter++) {
+		num[iter] = random() % num_range;
 	}
-
-	printf("generated random num: \n");
-	for (i = 0; i < num_size; i++) {
-		printf("%d\t", num[i]);
-		counter++;
-		if (counter % 10 == 0) {
-			printf("\n");
-		}
-	}
-	printf("\n");
 
 	return 0;
 }
 
-int get_num(int sort[])
+int get_num(int sort[], int num)
 {
-	int loop_integer = 0;
+	int iter = 0;
 
-	printf("input %d numbers: \n", NUMBER_SIZE);
-	for (loop_integer = 0; loop_integer < NUMBER_SIZE; loop_integer++) {
-		scanf("%d", &sort[loop_integer]);
+	printf("input %d numbers: \n", num);
+	for (iter = 0; iter < num; iter++) {
+		scanf("%d", &sort[iter]);
 	}
 
 	return 0;
 }
 
 /* buble sort 
- *
+ * @sort: 	array needed to be sorted.
+ * @num_size:	how many number to sort.
  */
-int buble_sort(int sort[])
+int buble_sort(int sort[], int num_size)
 {
 	int i = 0;
 	int j = 0;
 	int temp = 0;
-	int counter = 0;
 
-	for (i = 0; i < NUMBER_SIZE - 1; i++) { 
-		for (j = 0; j < NUMBER_SIZE - i - 1; j++) {
+	for (i = 0; i < num_size - 1; i++) { 
+		for (j = 0; j < num_size - i - 1; j++) {
 			if (sort[j] > sort[j + 1]) {
 				temp = sort[j];
 				sort[j] = sort[j + 1];
@@ -246,7 +194,8 @@ int buble_sort(int sort[])
 	}
 
 	printf("\nbuble sort done: \n");
-	for (i = 0; i < NUMBER_SIZE; i++) {
+	int counter = 0;
+	for (i = 0; i < num_size; i++) {
 		printf("%d\t", sort[i]);
 		if (++counter % 10 == 0) {
 			printf("\n");
@@ -257,6 +206,12 @@ int buble_sort(int sort[])
 	return 0;
 }
 
+/* merge sort
+ * input parameters:
+ *      @sort: 		the array needed to be sorted
+ *      @start_index: 	sorted from where
+ *      @end_index: 	sort to where
+ */
 int merge_sort(int sort[], int start_index, int end_index)
 {
 	int mid_index = (start_index + end_index) / 2;
@@ -270,37 +225,35 @@ int merge_sort(int sort[], int start_index, int end_index)
 	return 0;
 }
 
-int merge(int sort[], int start_index, int mid_index, int end_index)
+static int merge(int sort[], int start_index, int mid_index, int end_index)
 {
-	int buffer[end_index - start_index + 1];
-	int pos = 0;
-	int lpos = start_index;
-	int rpos = mid_index + 1;
-	printf("lpos = %d\n", lpos);
-	printf("rpos = %d\n", rpos);
+	int temp_array[end_index - start_index + 1];
+	int pos = 0;                /* position of temp_array's element*/
+	int lpos = start_index;     /* position of left sub array */
+	int rpos = mid_index + 1;   /* position of right sub array */
 
-	memset(buffer, 0, sizeof(buffer));
+	memset(temp_array, 0, sizeof(temp_array));
 
 	while (lpos <= mid_index && rpos <= end_index) {
 		if (sort[lpos] < sort[rpos]) {
-			buffer[pos++] = sort[lpos++];
+			temp_array[pos++] = sort[lpos++];
 		} else {
-			buffer[pos++] = sort[rpos++];
+			temp_array[pos++] = sort[rpos++];
 		}
 	}
 
 	/* the left numbers must bigger than others */
 	while (lpos <= mid_index) {
-		buffer[pos++] = sort[lpos++];
+		temp_array[pos++] = sort[lpos++];
 	}
 	while (rpos <= end_index) {
-		buffer[pos++] = sort[rpos++];
+		temp_array[pos++] = sort[rpos++];
 	}
 	/*******************************************/
 	
-	int iter = 0;
+	int iter = 0; /* iterator */
 	for (iter = 0; iter < pos; iter++) {
-		sort[start_index + iter] = buffer[iter];
+		sort[start_index + iter] = temp_array[iter];
 	}
 
 	return 0;
